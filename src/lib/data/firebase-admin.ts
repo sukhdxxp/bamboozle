eda9a70362b361b5c1dda831ca68b103ceae5224
Sukhdeep Singh <sukhdeep.singh@booking.com>
2023-07-09 15:13:34 +0200

    Updated private key

--- src/lib/data/firebase-admin.ts
+++ src/lib/data/firebase-admin.ts
@@ -1,14 +1,13 @@
 import admin from "firebase-admin";
 import { cert } from "firebase-admin/app";
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string);
 
 try {
   admin.initializeApp({
     credential: cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
-        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
-        : undefined,
+      privateKey: privateKey,
     }),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DB_URI,
   });
