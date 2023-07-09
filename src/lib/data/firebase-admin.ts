b5c5486853a8c09eeb618820e89e649b377436bb
Sukhdeep Singh <sukhdeep.singh@booking.com>
2023-07-09 15:53:06 +0200

    Updated private key

--- src/lib/data/firebase-admin.ts
+++ src/lib/data/firebase-admin.ts
@@ -2,17 +2,19 @@ import admin from "firebase-admin";
 import { cert } from "firebase-admin/app";
 const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string);
 
-try {
-  admin.initializeApp({
-    credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: privateKey.replace(/\\n/g, "\n"),
-    }),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DB_URI,
-  });
-} catch (err) {
-  console.log("Firebase admin initialization error", err);
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: privateKey.replace(/\\n/g, "\n"),
+      }),
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DB_URI,
+    });
+  } catch (err) {
+    console.log("Firebase admin initialization error", err);
+  }
 }
 
 const firebaseAdminAuth = admin.auth();
